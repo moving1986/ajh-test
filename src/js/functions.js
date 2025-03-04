ca4ed53b156cc92aab76864d26cabd56e91ff01f
Alexey Mezentsev <aleksei_mezencev@mail.ru>
2025-03-04 19:22:55 +0700

    init

---
+++ src/js/functions.js
@@ -0,0 +1,16 @@
+export function isValid(cardNumber) {
+    const digits = cardNumber.toString().split('').map(Number);
+  
+    let sum = 0;
+    const parity = digits.length % 2;
+  
+    for (let i = 0; i < digits.length - 1; i++) {
+      if (i % 2 !== parity) {
+        sum += digits[i];
+      } else if (digits[i] > 4) {
+        sum += 2 * digits[i] - 9;
+      } else {
+        sum += 2 * digits[i];
+      }
+    }
+}
